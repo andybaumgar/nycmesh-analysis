cb88b51caccb9e22c4630932c92c1ccfe3a638ab
Andy B <andybaumgar@gmail.com>
2023-06-13 17:31:42 -0400

    Update tp400zc_influx_data_logger.py

--- analysis/siklu_alignment/tp400zc_influx_data_logger.py
+++ analysis/siklu_alignment/tp400zc_influx_data_logger.py
@@ -1,13 +1,51 @@
 import tp4000zc
 from time import sleep
 
-port = '/dev/cu.usbserial-10'
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from influxdb import InfluxDBClient
+from datetime import datetime
+import json
 
+port = '/dev/cu.usbserial-10'
 dmm = tp4000zc.Dmm(port=port)
+
+influx_client = InfluxDBClient('localhost', 8086, 'admin', 'admin', 'siklu')
+
+def get_voltage():
+    val = dmm.read()
+    print(val)
+    return val
+
+
+def create_influx_post(self, measurements):
+    json_body = []
+    current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    for key, value in measurements.items():
+        json_part = {
+            "measurement": key,
+            "time": current_time,
+            "fields": {
+                "value": value
+            }
+        }
+
+        json_body.append(json_part)
+
+    return json_body
+
+def record_data(self):  
+
+    data = {"voltage": get_voltage()}
+    
+    post = self.create_influx_post(data)
+    influx_client.write_points(post)
+    
+    self.send_response(200)
+    self.end_headers()
 
 while(True):
     sleep(.1)
-    val = dmm.read()
-    print(val.numericVal) # and the numeric value
+    record_data()
 
 dmm.close()
