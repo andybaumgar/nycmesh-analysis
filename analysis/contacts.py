77f696579eba1a9584dd573310f31dfdef736b04
andybaumgar <andybaumgar@gmail.com>
2023-07-12 21:45:37 -0400

    add get_downstream_nns()

--- analysis/contacts.py
+++ analysis/contacts.py
@@ -2,12 +2,14 @@ from dotenv import load_dotenv
 import os
 
 import mesh_database_client
+from mesh_utils import get_downstream_nns
 
 spreadsheet_id = os.environ.get("SPREADSHEET_ID")
 database_client = mesh_database_client.DatabaseClient(spreadsheet_id=spreadsheet_id)
 
 def hub_to_emails(hub_nn, paste_format=True):
     linked_nns = database_client.nn_to_linked_nn(hub_nn)
+    linked_nns = [database_client.get_nn(x) for x in linked_nns]
 
     df = database_client.signup_df.copy()
 
@@ -20,5 +22,14 @@ def hub_to_emails(hub_nn, paste_format=True):
     return emails
 
 if __name__ == "__main__":
-    emails = hub_to_emails(2274)
-    print(emails)
+    nns_basic = database_client.nn_to_linked_nn(2274)
+    nns_basic = [database_client.get_nn(x) for x in nns_basic]
+    nns_shortest_path = get_downstream_nns(2274, database_client)
+    # print(nns_basic)
+    # print(nns_shortest_path)
+
+    difference = list(set(nns_shortest_path)-set(nns_basic))
+    print(difference)
+
+    # emails_basic = hub_to_emails(2274)
+    # print(emails)
--- analysis/mesh_utils.py
+++ analysis/mesh_utils.py
@@ -1,4 +1,7 @@
 import re
+import mesh_database_client
+import networkx as nx
+import os
 
 def nn_from_string(input_string):
     matches = re.findall("(\d{3,})", input_string)
@@ -13,3 +16,40 @@ def nn_to_ip(nn):
     ip_third_octet=int((nn-ip_fourth_octet)/100)
     ip = f"10.69.{ip_third_octet}.{ip_fourth_octet}"
     return ip
+
+def get_downstream_nns(hub_nn:int, database_client):
+    
+    # TODO: look through all supernodes
+    supernode_nns = [1932, 227, 713]
+
+    signup_df = database_client.signup_df.copy()
+    active_nodes = signup_df[signup_df['Status'].isin(['Installed', 'NN assigned'])]
+    active_nodes = active_nodes[active_nodes['NN'] != 0]
+    
+    links_df = database_client.links_df.copy()
+    links_df = links_df[links_df['status'] != 'dead']
+
+    # convert IN to NN
+    for field in ['to', 'from']:        
+        links_df[field] = links_df[field].apply(lambda x:database_client.get_nn(x))
+    
+    graph = nx.from_pandas_edgelist(links_df, 'to', 'from')
+
+    downstream_nns = []
+    for nn in active_nodes['NN']:
+        try:
+            path = list(nx.shortest_path(graph, nn, 227))
+            if hub_nn in path:
+                downstream_nns.append(nn) 
+        except Exception as e:
+            pass
+
+    return downstream_nns
+
+if __name__ == "__main__":
+ 
+    spreadsheet_id = os.environ.get("SPREADSHEET_ID")
+    database_client = mesh_database_client.DatabaseClient(spreadsheet_id=spreadsheet_id)
+
+    nns = get_downstream_nns(2274, database_client)
+    print(nns)
