dba23cd96977502737ac367d6bbf4cb41eff3dbe
andybaumgar <andybaumgar@gmail.com>
2023-07-14 17:51:09 -0400

    contacts app

--- analysis/contacts.py
+++ analysis/contacts.py
@@ -4,16 +4,9 @@ import os
 import mesh_database_client
 from mesh_utils import get_downstream_nns
 
-spreadsheet_id = os.environ.get("SPREADSHEET_ID")
-database_client = mesh_database_client.DatabaseClient(spreadsheet_id=spreadsheet_id)
-
-def hub_to_emails(hub_nn, paste_format=True):
-    linked_nns = database_client.nn_to_linked_nn(hub_nn)
-    linked_nns = [database_client.get_nn(x) for x in linked_nns]
-
+def nns_to_emails(nns, database_client, paste_format=True):
     df = database_client.signup_df.copy()
-
-    df = df[df['NN'].isin(linked_nns)]
+    df = df[df['NN'].isin(nns)]
     emails = df['Email'].tolist()
 
     if paste_format:
@@ -21,15 +14,34 @@ def hub_to_emails(hub_nn, paste_format=True):
 
     return emails
 
+# def hub_to_emails(hub_nn, paste_format=True):
+#     linked_nns = database_client.nn_to_linked_nn(hub_nn)
+#     linked_nns = [database_client.get_nn(x) for x in linked_nns]
+
+#     df = database_client.signup_df.copy()
+
+#     df = df[df['NN'].isin(linked_nns)]
+#     emails = df['Email'].tolist()
+
+#     if paste_format:
+#         return " ".join(emails)
+
+#     return emails
+
 if __name__ == "__main__":
-    nns_basic = database_client.nn_to_linked_nn(2274)
-    nns_basic = [database_client.get_nn(x) for x in nns_basic]
-    nns_shortest_path = get_downstream_nns(2274, database_client)
+    spreadsheet_id = os.environ.get("SPREADSHEET_ID")
+    database_client = mesh_database_client.DatabaseClient(spreadsheet_id=spreadsheet_id)
+    # nns_basic = database_client.nn_to_linked_nn(2274)
+    # nns_basic = [database_client.get_nn(x) for x in nns_basic]
+    # nns_shortest_path = get_downstream_nns(2274, database_client)
     # print(nns_basic)
     # print(nns_shortest_path)
+    nns = [1158, 5639, 263, 3721, 4745, 2699, 1430, 407, 3353, 410, 2590, 294, 2983, 2090, 4026, 6978, 454, 465, 7764, 4693, 348, 3175, 7016, 2537, 2539, 7535, 239, 4337, 369, 6004, 246, 374, 378, 507, 2814]
+    emails = nns_to_emails(nns, database_client)
+    print(emails)
 
-    difference = list(set(nns_shortest_path)-set(nns_basic))
-    print(difference)
+    # difference = list(set(nns_shortest_path)-set(nns_basic))
+    # print(difference)
 
     # emails_basic = hub_to_emails(2274)
     # print(emails)
--- analysis/utils.py
+++ analysis/utils.py
@@ -1,6 +1,50 @@
 from pathlib import Path
+import re
+import mesh_database_client 
+import networkx as nx
 
 def get_data_file_path(data_filename):
     data_path_object = Path(__file__).parent.parent / 'data'
     data_file_path =  str(data_path_object / data_filename)
     return data_file_path
+
+def get_distance_miles(location_1, location_2):
+    x1 = location_1['Latitude']
+    y1 = location_1['Longitude']
+    x2 = location_2['Latitude']
+    y2 = location_2['Longitude']
+
+    distance_degrees = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+    approximate_distance_miles = distance_degrees * 69
+
+    return approximate_distance_miles
+
+def nns_from_string(input_string):
+    matches = re.findall("(\d{3,})", input_string)
+
+    if not matches or len(matches) <= 1:
+        return None
+    else:
+        matches = [int(match) for match in matches]
+        return matches[0], matches[1]
+    
+def nn_to_latitude(nn, database_client):
+    try:
+        return database_client.nn_to_location(nn)['Latitude']
+    except:
+        return None
+
+def nn_to_longitude(nn, database_client):
+    try:
+        return database_client.nn_to_location(nn)['Longitude']
+    except:
+        return None
+
+def get_link_distance(nn_1, nn_2, database_client):
+    location_1 = database_client.nn_to_location(nn_1)
+    location_2 = database_client.nn_to_location(nn_2)
+
+    distance_miles = get_distance_miles(location_1, location_2)
+
+    return distance_miles
+
