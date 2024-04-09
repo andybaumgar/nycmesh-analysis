d08ce44cecdcdb3cbbfedc67d2813e701265cdbf
Andy Baumgartner <andybaumgar@gmail.com>
2024-04-09 15:08:22 -0400

    Enable data saving

--- analysis/link_nyc_map.py
+++ analysis/link_nyc_map.py
@@ -8,107 +8,110 @@ from shapely.geometry import Point
 import os
 import plotly.express as px
 import plotly.graph_objects as go
-# 
+import pickle
 
-# get data folder in parent of current folder
-data_folder = Path(__file__).parent.parent/'data_small'
-kiosk_df = pd.read_json(str(data_folder/'kiosks.json'))
-node_df = pd.read_json(str(data_folder/'nodes.json'))
+data_small_folder = Path(__file__).parent.parent/'data_small'
+data_folder = Path(__file__).parent.parent/'data'
 
-kiosk_df['latitude'] = kiosk_df['coordinates'].apply(lambda x: x[1])
-kiosk_df['longitude'] = kiosk_df['coordinates'].apply(lambda x: x[0])
-node_df['latitude'] = node_df['coordinates'].apply(lambda x: x[1])
-node_df['longitude'] = node_df['coordinates'].apply(lambda x: x[0])
+def get_nodes_near_kiosks():
+    kiosk_df = pd.read_json(str(data_small_folder/'kiosks.json'))
+    node_df = pd.read_json(str(data_small_folder/'nodes.json'))
+    
+    node_df = node_df[node_df['status'] != 'Installed']
 
-
-# calculate distance
-
-# print(kiosk_df.head())
-# print(node_df.head())
-
-# Assuming kiosk_df has columns 'kiosk_id', 'latitude', 'longitude'
-# and node_df has columns 'node_id', 'latitude', 'longitude'
-
-# Convert kiosk_df to GeoDataFrame
-kiosk_geometry = [Point(xy) for xy in zip(kiosk_df['longitude'], kiosk_df['latitude'])]
-kiosk_gdf = gpd.GeoDataFrame(kiosk_df, geometry=kiosk_geometry, crs='EPSG:4326')
-
-# Convert node_df to GeoDataFrame
-node_geometry = [Point(xy) for xy in zip(node_df['longitude'], node_df['latitude'])]
-node_gdf = gpd.GeoDataFrame(node_df, geometry=node_geometry, crs='EPSG:4326')
-
-# Set coordinate reference system (CRS) if it's not set already
-# kiosk_gdf.crs = 'EPSG:4326'
-# node_gdf.crs = 'EPSG:4326'
-
-# Buffer nodes with 50m buffer distance
-distance_meters = 50
-distance_degrees = distance_meters / 111139
-kiosk_buffer = kiosk_gdf.geometry.buffer(distance_degrees)
-
-# Check if any kiosks intersect with node buffers
-nodes_within_50m = node_gdf[node_gdf.geometry.intersects(kiosk_buffer.unary_union)]
-
-# Print kiosks within 50m of a node
-print("Nodes within 50m of a kiosk:")
-print(nodes_within_50m)
+    kiosk_df['latitude'] = kiosk_df['coordinates'].apply(lambda x: x[1])
+    kiosk_df['longitude'] = kiosk_df['coordinates'].apply(lambda x: x[0])
+    node_df['latitude'] = node_df['coordinates'].apply(lambda x: x[1])
+    node_df['longitude'] = node_df['coordinates'].apply(lambda x: x[0])
 
 
+    kiosk_geometry = [Point(xy) for xy in zip(kiosk_df['longitude'], kiosk_df['latitude'])]
+    kiosk_gdf = gpd.GeoDataFrame(kiosk_df, geometry=kiosk_geometry, crs='EPSG:4326')
+
+    node_geometry = [Point(xy) for xy in zip(node_df['longitude'], node_df['latitude'])]
+    node_gdf = gpd.GeoDataFrame(node_df, geometry=node_geometry, crs='EPSG:4326')
+
+    distance_meters = 50
+    distance_degrees = distance_meters / 111139
+    kiosk_buffer = kiosk_gdf.geometry.buffer(distance_degrees)
+
+    nodes_near_kiosks = node_gdf[node_gdf.geometry.intersects(kiosk_buffer.unary_union)]
+
+    print('non-"Installed" Nodes within 50m of a kiosk:')
+    print(nodes_near_kiosks)
+
+    nodes_near_kiosk_hashable = pd.read_json(nodes_near_kiosks.to_json())
+
+    data = {"nodes_near_kiosks":nodes_near_kiosks, "kiosk_df":kiosk_df, "node_df":node_df}
+
+    with open(data_folder/'nodes_near_kiosks.pkl', 'wb') as f:
+        pickle.dump(data, f)
+
+    return nodes_near_kiosks, kiosk_df, node_df
+
+def map_nodes_near_kiosks(nodes_near_kiosks, kiosk_df, node_df):
+
+    fig = go.Figure(go.Scattermapbox())
+
+    fig.add_trace(go.Scattermapbox(
+        mode='markers',
+        lon=node_df['longitude'],
+        lat=node_df['latitude'],
+        marker=dict(
+            size=10,
+            color='blue',
+        ),
+        name='Nodes'
+    ))
 
 
-# Create a base Mapbox figure
-fig = go.Figure(go.Scattermapbox())
+    fig.add_trace(go.Scattermapbox(
+        mode='markers',
+        lon=kiosk_df['longitude'],
+        lat=kiosk_df['latitude'],
+        marker=dict(
+            size=10,
+            color='brown',
+        ),
+        name='Kiosks'
+    ))
 
-# Add nodes to the figure
-fig.add_trace(go.Scattermapbox(
-    mode='markers',
-    lon=node_df['longitude'],
-    lat=node_df['latitude'],
-    marker=dict(
-        size=10,
-        color='blue',
-    ),
-    name='Nodes'
-))
+    fig.add_trace(go.Scattermapbox(
+        mode='markers',
+        lon=nodes_near_kiosks['longitude'],
+        lat=nodes_near_kiosks['latitude'],
+        marker=dict(
+            size=7,
+            color='yellow',
+        ),
+        name='Nodes within 50m of a kiosk'
+    ))
 
-# Add original kiosks to the figure
-fig.add_trace(go.Scattermapbox(
-    mode='markers',
-    lon=kiosk_df['longitude'],
-    lat=kiosk_df['latitude'],
-    marker=dict(
-        size=10,
-        color='brown',
-    ),
-    name='Original Kiosks'
-))
-
-# Add found kiosks within 50m of a node to the figure
-fig.add_trace(go.Scattermapbox(
-    mode='markers',
-    lon=nodes_within_50m['longitude'],
-    lat=nodes_within_50m['latitude'],
-    marker=dict(
-        size=7,
-        color='yellow',
-    ),
-    name='Kiosks within 50m of a Node'
-))
-
-# Update layout to set Mapbox parameters
-fig.update_layout(
-    hovermode='closest',
-    mapbox=dict(
-        style="carto-positron",
-        center=dict(lon=node_df['longitude'].mean(), lat=node_df['latitude'].mean()),
-        zoom=10
+    fig.update_layout(
+        hovermode='closest',
+        mapbox=dict(
+            style="carto-positron",
+            center=dict(lon=node_df['longitude'].mean(), lat=node_df['latitude'].mean()),
+            zoom=10
+        )
     )
-)
 
+    fig.update_layout(title_text='Uninstalled Nodes within 50m of a kiosk')
 
-# Show the figure
-fig.show()
+    fig.show()
 
+def get_data_from_file():
+    with open(data_folder/'nodes_near_kiosks.pkl', 'rb') as f:
+        data = pickle.load(f)
+        nodes_near_kiosks = data["nodes_near_kiosks"]
+        kiosk_df = data["kiosk_df"]
+        node_df = data["node_df"]
+    return nodes_near_kiosks, kiosk_df, node_df
 
+# nodes_near_kiosks, kiosk_df, node_df = get_nodes_near_kiosks()
+
+nodes_near_kiosks, kiosk_df, node_df = get_data_from_file()
+
+map_nodes_near_kiosks(nodes_near_kiosks, kiosk_df, node_df)
 
 # %%
